247e258e520a397611079032cca5f96d74b6b1f8
Liam Dupeyron <dupeyron09@berkeley.edu>
2024-06-25 22:39:47 -0700

    Added paddles

--- pong.py
+++ pong.py
@@ -1,6 +1,9 @@
 # A simple Pong terminal game for when I have no internet
 # By Liam Dupeyron
 
+from sre_parse import WHITESPACE
+from struct import pack
+from tkinter import LEFT
 import pygame as pg
 
 # pygame setup
@@ -9,13 +12,52 @@ WIDTH, HEIGHT = 700, 500
 WIN = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption('Pong')
 
+FPS = 60
+
+WHITE = (225, 225, 225)
+BLACK = (0, 0, 0)
+
+PADDLE_WIDTH, PADDLE_HEIGHT = 10, 90
+
+class Paddle:
+    COLOR = WHITE
+
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def draw(self, win):
+        pg.draw.rect(win, self.COLOR, (self.x, self.y, self.width, self.height))
+         
+
+
+
+def draw(win, paddles):
+    win.fill('black')
+
+    for paddle in paddles:
+        paddle.draw(win)
+
+    pg.display.update()
+    
+
 def main():
     run = True
+    clock = pg.time.Clock()
+
+    left_paddle = Paddle(10,(HEIGHT//2) - (PADDLE_HEIGHT//2), PADDLE_WIDTH, PADDLE_HEIGHT)
+    right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH,(HEIGHT//2) - (PADDLE_HEIGHT//2), PADDLE_WIDTH, PADDLE_HEIGHT)
 
     while run:
+        clock.tick(FPS)
+        draw(WIN, [left_paddle, right_paddle])
+    
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 run = FALSE
+                break
 
     pg.quit()
 
